c61a0e2890fe571dc8e63457d843f46fe6179f18
manuel perez <manuelperez.0000@gmail.com>
2023-11-30 00:52:40 -0400

    qwe

--- network/routerApi.js
+++ network/routerApi.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const users = require('../routes/users/users')
+/* const users = require('../routes/users/users') */
 const buildings = require('../routes/buildings/buildings')
 
 const upload = require('../routes/upload/upload')
 
 const routerApi = (app) => {
     const route = express.Router()
-    app.use('/api/v1', route)
-    route.use('/users', users)
+    app.use('/api/v1', route)/* 
+    route.use('/users', users) */
     route.use('/buildings', buildings)
     route.use('/upload', upload)
 }
