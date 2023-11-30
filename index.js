0eafe1475bc1dbff146b36716d4a4eede4df2eed
kaltre10 <kaltre10@gmail.com>
2023-11-29 22:53:03 -0500

    5

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Welcome hello world')
 })
 
+app.get('/users/api/v1/', (req, res) => {
+    res.send('/users/api/v1/')
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('listening on port', PORT))
